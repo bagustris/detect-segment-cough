3c9bbd3b8961b836a979f5a67ca8c6cbc9457d7a
Bagus Tris Atmaja <bagustris@yahoo.com>
2022-03-08 21:32:01 +0900

    prettify

--- detect_cough.py
+++ detect_cough.py
@@ -12,27 +12,30 @@ from scipy.io import wavfile
 import pickle
 import argparse
 
+
 def main(input_file):
     """
     Detect cough in a given audio file
     Inputs:
         input_file: (str) path to audio file
     Outputs:
-        result: (float) probability that a given file is a cough 
+        result: (float) probability that a given file is a cough
     """
     # data_folder = './sample_recordings'
-    model = pickle.load(open(os.path.join('./models', 'cough_classifier'), 'rb'))
-    scaler = pickle.load(open(os.path.join('./models','cough_classification_scaler'), 'rb'))
+    model = pickle.load(open(os.path.join('./models', 'cough_classifier'),
+        'rb'))
+    scaler = pickle.load(open(os.path.join('./models',
+        'cough_classification_scaler'), 'rb'))
 
-    fs,x = wavfile.read(input_file)
+    fs, x = wavfile.read(input_file)
     prob = classify_cough(x, fs, model, scaler)
     print(f"{input_file} has probability of cough: {prob}")
     return prob
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', 
-                        help='Path to input audio file', 
+    parser.add_argument('-i', '--input',
+                        help='Path to input audio file',
                         required=True)
     args = parser.parse_args()
     main(args.input)
